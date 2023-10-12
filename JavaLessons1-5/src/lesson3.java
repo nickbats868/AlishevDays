703ddf1ec256e96c2da2a627b448fababdef631b
Nick <mikalaimiazhenny@gmail.com>
2023-10-12 12:51:48 +0200

    Удалена ненужная папка

--- JavaLessons1-5/src/lesson3.java
+++
@@ -1,7 +0,0 @@
-public class lesson3 {
-    public static void main(String[]args) {
-        int x =28;
-        String s = "Hellp";
-        System.out.println("My dick is " + x  + " Johnny");
-    }
-}
