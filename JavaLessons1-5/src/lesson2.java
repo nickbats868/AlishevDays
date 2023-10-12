7da0023cc66f51a050384b9a625c6fd04531bf89
Nick <mikalaimiazhenny@gmail.com>
2023-10-12 12:59:32 +0200

    Deleted folder has been restored

---
+++ JavaLessons1-5/src/lesson2.java
@@ -0,0 +1,16 @@
+public class lesson2 {
+    public static void main(String[]args){
+        int myInt = 521;
+        short myShort = 4222;
+        long myLong = 23333333;
+
+        double myDouble = 233.44;
+        float myFloat = 32.134f;
+        System.out.println(myInt);
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; //-128 - 127
+    }
+}
