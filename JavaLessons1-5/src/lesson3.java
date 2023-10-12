72699de590c4f7aa5cd5538e1f8832b6a6e22dc0
Nick <mikalaimiazhenny@gmail.com>
2023-10-12 12:36:19 +0200

    Рабочие задания 1 и 2 дня + установил гит и запушил это дерьмо

---
+++ JavaLessons1-5/src/lesson3.java
@@ -0,0 +1,7 @@
+public class lesson3 {
+    public static void main(String[]args) {
+        int x =28;
+        String s = "Hellp";
+        System.out.println("My dick is " + x  + " Johnny");
+    }
+}
